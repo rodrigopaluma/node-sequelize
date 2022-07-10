2346a4a0c8aa4fe40dc2ee52b5f9cba4863553f4
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-10 06:40:11 -0300

    Salvando e Resgatando dados relacionados.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars');
 const conn = require('./db/conn');
 
 const User = require('./models/User');
+const Address = require('./models/Address');
 
 const app = express();
 
@@ -53,8 +54,8 @@ app.post('/users/delete/:id', async (req, res) => {
 // Rota editar usuário
 app.get('/users/edit/:id', async (req, res) => {
     const id = req.params.id;
-    const user = await User.findOne({raw: true, where: {id: id}});
-    res.render('useredit', {user});
+    const user = await User.findOne({include: Address, where: {id: id}});
+    res.render('useredit', {user: user.get({plain: true})});
 })
 
 // Update de Usuário
@@ -74,12 +75,34 @@ app.post('/users/update', async (req, res) => {
     res.redirect('/');
 })
 
+// Criar Endereço
+app.post('/address/create', async (req, res) => {
+
+    const UserId = req.body.UserId;
+    const street = req.body.street;
+    const number = req.body.number;
+    const city = req.body.city;
+
+    const address = {
+        UserId,
+        street,
+        number,
+        city
+    }
+
+    await Address.create(address);
+    res.redirect(`/users/edit/${UserId}`);
+})
+
 // Home
 app.get('/', async (req, res)=>{
     const users = await User.findAll({ raw: true});
     res.render('home',{users});
 })
 
-conn.sync().then(()=>{
-    app.listen(3000);
+conn
+    .sync()
+    //.sync({ force: true })
+    .then(()=> {
+        app.listen(3000);
 }).catch((err)=> console.log(err));
